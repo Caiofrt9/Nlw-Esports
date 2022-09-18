b2c43b5b1d2df2202453e6912ff8b1d4bd02f8ae
Caio F <silvacaio295@gmail.com>
2022-09-18 13:13:52 -0300

    finalizando landing page web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,11 +2,16 @@
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/fundo.png ')",
         'nlw-gradient':
-          'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%)'
+          'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)'
       }
     }
   },
